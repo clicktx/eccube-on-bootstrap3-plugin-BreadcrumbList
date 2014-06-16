7f8fbd62a913d29fb3597ca4eed11abf8e94f955
clicktx <clicktx@gmail.com>
2014-06-16 18:02:49 +0900

    Change プラグイン情報を更新

--- plugin_info.php
+++ plugin_info.php
@@ -11,14 +11,14 @@
 class plugin_info
 {
     public static $PLUGIN_CODE       = "BreadcrumbList";
-    public static $PLUGIN_NAME       = "全ページ対応パンくずリスト表示プラグイン";
+    public static $PLUGIN_NAME       = "全ページ対応パンくずリスト表示プラグイン for EC-CUBE on BootStrap3";
     public static $CLASS_NAME        = "BreadcrumbList";
     public static $PLUGIN_VERSION    = "0.3";
     public static $COMPLIANT_VERSION = "2.12, 2.13";
     public static $AUTHOR            = "Nobuhiko Kimoto";
-    public static $DESCRIPTION       = "パンくずリストを生成する";
-    public static $PLUGIN_SITE_URL   = "http://nob-log.info/2013/12/17/ec-cube-plugin-breadcrumblist/";
-    public static $AUTHOR_SITE_URL   = "http://nob-log.info/";
+    public static $DESCRIPTION       = "パンくずリストを生成する。テンプレート「EC-CUBE on BootStrap3」に最適化済。";
+    public static $PLUGIN_SITE_URL   = "https://github.com/clicktx/eccube-on-bootstrap3-plugin-BreadcrumbList";
+    public static $AUTHOR_SITE_URL   = "http://perl.no-tubo.net";
     public static $HOOK_POINTS       =  array(
     );
     public static $LICENSE           = "LGPL";
